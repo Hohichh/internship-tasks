4a3dc283d3dfee5d8ea9175592afa65434ec5ec1
Hohichh <verkovich2005@gmail.com>
2025-10-20 15:53:16 +0300

    fixed alias for name column in employees-earn-more-than-managers.sql

--- SQL/employees-earn-more-than-managers.sql
+++ SQL/employees-earn-more-than-managers.sql
@@ -8,7 +8,7 @@ WHERE (employee.salary > manager.salary)
 
 -- 2nd option 
 -- much worse in terms of speed
-SELECT name FROM Employee e
+SELECT name as Employee FROM Employee e
 WHERE salary > (
     SELECT salary FROM Employee m
     WHERE e.managerId = m.id
